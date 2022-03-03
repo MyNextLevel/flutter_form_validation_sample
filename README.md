53711c1808b7b010212320af6f847f85760bd8c5
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2022-03-03 12:37:08 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Flutter form validating sample
---
+++ android/app/src/main/kotlin/com/donaboyev/flutter_form_validating_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.donaboyev.flutter_form_validating_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
